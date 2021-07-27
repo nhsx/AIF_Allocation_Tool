e52de03502fe0467db728367133db59869f90f7e
Mthokozisi Santu <mthokozisisantu@mthokozisis-air.home>
2021-07-27 17:00:32 +0100

    Initial commit

---
+++ allocation_tool.py
@@ -0,0 +1,48 @@
+import streamlit as st
+import pandas as pd
+
+# App appearance
+st.title("Place Based Allocation Tool")
+
+
+# Load data and cache
+@st.cache
+def get_data():
+    path = "~/PycharmProjects/pythonProject3/gp_practice_weighted_population_by_ics v2.xlsx"
+    return pd.read_excel(path, 1, 0, usecols="B,F,H,J,L,M,N:AC")
+
+
+data = get_data()
+data = data.rename(columns={"Region21_7": "Region", "STP21_42": "ICS", "GP practice name": "practice_name"})
+
+ics = data['ICS'].drop_duplicates()
+ics_choice = st.sidebar.selectbox("Select your ICS:", ics)
+ccgs = list(data['CCG19'].loc[data['ICS'] == ics_choice])
+pcns = list(data['PCN_name'].loc[data['ICS'] == ics_choice])
+practices = list(data["Practice"].loc[data["ICS"] == ics_choice])
+practice_choice = st.sidebar.multiselect("Select practices", practices)
+place_name = st.text_input("Place Name/Number", "group 1")
+
+
+def new_df(df, column, values):
+    """"
+    Output a new dataframe based on the rows in the original dataframe passed to the function
+
+    Args:
+        df (pandas Dataframe): The dataframe containing the original values
+        column (str): The column name in the dataframe from which the values are derived
+        values (list): A list of column values that are contained in the dataframe whose rows will make up the new dataframe
+
+    Returns:
+        pandas Dataframe: The new dataframe that can be manipulated and aggregated
+
+    """
+    # Subset the dataframe
+    new_data: object = df[df[column].isin(values)]
+    return new_data
+
+
+result1 = new_df(data, 'Practice', practice_choice)
+result1 = result1.assign(Place=place_name)
+
+st.write(result1)
