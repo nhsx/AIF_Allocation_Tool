9b365ad31159abdf956e32b13c538b83adf81e7e
Craig Shenton <craig.shenton@nhsx.nhs.uk>
2021-12-13 22:07:35 +0000

    metrics

--- app/dashboard.py
+++ app/dashboard.py
@@ -1,13 +1,30 @@
-import streamlit as st
-from streamlit.elements.legacy_data_frame import LegacyDataFrameMixin
+# -------------------------------------------------------------------------
+# Copyright (c) 2021 NHS England and NHS Improvement. All rights reserved.
+# Licensed under the MIT License. See license.txt in the project root for
+# license information.
+# -------------------------------------------------------------------------
 
+"""
+FILE:           dashboard.py
+DESCRIPTION:    streamlit weighted capitation tool
+USAGE:
+CONTRIBUTORS:   
+CONTACT:        
+CREATED:        2021
+VERSION:        0.0.2
+"""
+
+# Libs
+# -------------------------------------------------------------------------
+# 3rd party:
+import streamlit as st
 import pandas as pd
 import utils
 
-# Temporary prototype notice
+# Markdown
+# -----------------------------------------------------
 st.markdown("PROTOTYPE UNDER DEVELOPMENT - Last Updated 9th December 2021")
 
-# App appearance
 st.image(
     "../images/nhs_logo.png", caption=None,
 )
@@ -24,73 +41,179 @@ st.markdown(
 )
 
 # Import Data
+# -----------------------------------------------------
 data = utils.get_data()
 ics = utils.get_sidebar(data)
 
 # SIDEBAR
+# -----------------------------------------------------
 ics_choice = st.sidebar.selectbox("Select your ICS:", ics, help="Select an ICS")
 ccg_filter = st.sidebar.checkbox("Filter by CCG")
 if ccg_filter:
-    ccg = data["CCG19"].loc[data["ICS"] == ics_choice].unique().tolist()
+    ccg = data["CCG name"].loc[data["ICS name"] == ics_choice].unique().tolist()
     ccg_choice = st.sidebar.selectbox("Select your CCG:", ccg, help="Select a CCG")
-    practices = list(data["Practice"].loc[data["CCG19"] == ccg_choice])
+    practices = list(data["practice_display"].loc[data["CCG name"] == ccg_choice])
 else:
-    practices = list(data["Practice"].loc[data["ICS"] == ics_choice])
+    practices = list(data["practice_display"].loc[data["ICS name"] == ics_choice])
 
 practice_choice = st.sidebar.multiselect(
     "Select practices",
     practices,
     help="Select GP Practices to aggregate into a single defined 'place'",
 )
-# BODY
-place_name = st.text_input(
-    "Place Name", "Group 1", help="Give your defined place a name to identify it"
+place_name = st.sidebar.text_input(
+    "Name your Group", "Group 1", help="Give your defined place a name to identify it"
 )
-
-# BUTTONS
-
-if st.button("Add Selection to Group", help="s", key="output",):
+if st.sidebar.button("Save Group", help="s", key="output",):
     if [place_name] not in st.session_state:
         st.session_state[place_name] = practice_choice
 
-if st.button("Reset Group", key="output",):
+if st.sidebar.button("Reset Group", key="output",):
     del st.session_state[place_name]
+
+# BODY
+# -----------------------------------------------------
+option = st.selectbox("Selected Place", ("test", "test"))
+
+st.write("You selected:", option)
 
 st.session_state
 
+cols = [
+    "Place Name",
+    "GP practice code",
+    "GP practice name",
+    "G&A Index",
+    "Community Index",
+    "Mental Health Index",
+    "Maternity Index",
+    "HCHS Index",
+    "Market Forces Factor Index",
+    "EACA Index",
+    "Prescribing Index",
+    "AM Index",
+    "Overall Index",
+    "GP pop",
+    "Weighted G&A pop",
+    "Weighted Community pop",
+    "Weighted Mental Health pop",
+    "Weighted Maternity pop",
+    "Weighted HCHS pop",
+    "Weighted Market Forces Factor pop",
+    "Weighted EACA pop",
+    "Weighted Prescribing pop",
+    "Weighted AM pop",
+    "Target exc remote (£k)",
+    "Target inc remote (£k)",
+    "Overall Weighted pop",
+    "Region ODS code",
+    "Region name",
+    "ICS ODS code",
+    "ICS ONS code",
+    "ICS name",
+    "CCG ODS code",
+    "CCG name",
+    "PCN ODS code",
+    "PCN code",
+]
+
 aggregations = {
-    "GP_pop": "sum",
-    "WP_G&A": "sum",
-    "WP_CS": "sum",
-    "WP_MH": "sum",
-    "WP_Mat": "sum",
-    "WP_HCHS": "sum",
-    "EACA_index": "mean",
-    "WP_Presc": "sum",
-    "WP_AM": "sum",
-    "WP_Overall": "sum",
+    "GP pop": "sum",
+    "Weighted G&A pop": "sum",
+    "Weighted Community pop": "sum",
+    "Weighted Mental Health pop": "sum",
+    "Weighted Maternity pop": "sum",
+    "Weighted HCHS pop": "sum",
+    "Weighted Market Forces Factor pop": "sum",
+    "Weighted EACA pop": "sum",
+    "Weighted Prescribing pop": "sum",
+    "Weighted AM pop": "sum",
+    "Target exc remote (£k)": "sum",
+    "Target inc remote (£k)": "sum",
+    "Overall Weighted pop": "sum",
 }
 
-query = "Practice == @place_practices"
+index_numerator = [
+    "Weighted G&A pop",
+    "Weighted Community pop",
+    "Weighted Mental Health pop",
+    "Weighted Maternity pop",
+    "Weighted HCHS pop",
+    "Weighted Market Forces Factor pop",
+    "Weighted EACA pop",
+    "Weighted Prescribing pop",
+    "Weighted AM pop",
+    "Overall Weighted pop",
+]
 
+index_names = [
+    "G&A Index",
+    "Community Index",
+    "Mental Health Index",
+    "Maternity Index",
+    "HCHS Index",
+    "Market Forces Factor Index",
+    "EACA Index",
+    "Prescribing Index",
+    "AM Index",
+    "Overall Index",
+]
 
-def aggregate(data, query, place_name, aggregations):
+gp_query = "practice_display == @place_practices"
+ics_query = "`ICS name` == @ics_choice"  # escape column names with backticks https://stackoverflow.com/a/56157729
+
+# aggregate on a query and set of aggregations
+def aggregate(data, query, name, on, aggregations):
     df = data.query(query)
-    df.insert(loc=0, column="Place Name", value=place_name)
-    df_group = df.groupby("Place Name").agg(aggregations)
+    if on not in df.columns:
+        df.insert(loc=0, column=on, value=name)
+    df_group = df.groupby(on).agg(aggregations)
+    df_group = df_group.astype(int)
     return df, df_group
+
+
+# calculate index of weighted populations
+def get_index(place_indices, ics_indices, index_names, index_numerator):
+    ics_indices[index_names] = ics_indices[index_numerator].div(
+        ics_indices["GP pop"].values, axis=0
+    )
+    place_indices[index_names] = (
+        place_indices[index_numerator]
+        .div(place_indices["GP pop"].values, axis=0)
+        .div(ics_indices[index_names].values, axis=0)
+    )
+    return place_indices, ics_indices
 
 
 if st.button("Calculate", help="s", key="output",):
     place_practices = st.session_state[place_name]
-    df_query, df_indices = aggregate(data, query, place_name, aggregations)
+    # get place aggregations
+    place_query, place_indices = aggregate(
+        data, gp_query, place_name, "Place Name", aggregations
+    )
+    st.write(ics_choice)
+    # get ICS aggregations
+    ics_query, ics_indices = aggregate(
+        data, ics_query, ics_choice, "ICS name", aggregations
+    )
+    # index calcs
+    place_indices1, ics_indices1 = get_index(
+        place_indices, ics_indices, index_names, index_numerator
+    )
 
-    df_indices.insert(loc=0, column="Place Name", value=place_name)
-    df_indices.insert(loc=0, column="Practice", value=place_name + " Totals")
-    df_indices = pd.concat([df_indices, df_query], axis=0, join="outer")
-    df_indices = df_indices[data.columns.tolist()]
-    df_indices.insert(loc=0, column="Place Name", value=place_name)
+    # print all data
+    ics_indices1.insert(loc=0, column="Group / ICS", value=ics_choice)
+    place_indices1.insert(loc=0, column="Group / ICS", value=place_name)
+    df_print = pd.concat(
+        [ics_indices1, place_indices1], axis=0, join="inner", ignore_index=True
+    )
     with st.container():
-        utils.write_table(df_indices)
+        utils.write_table(df_print)
 
-st.metric("Temperature", "70 °F", "1.2 °F")
+    for metric in index_names:
+        place_metric = round(place_indices1[metric][0].astype(float), 3)
+        ics_metric = round(ics_indices1[metric][0].astype(float), 3)
+        st.metric(
+            metric, place_metric, ics_metric, delta_color="normal",
+        )
+
--- app/utils.py
+++ app/utils.py
@@ -9,8 +9,35 @@ import numpy as np
 def get_data():
     path = "../data/wp_data.csv"  # file containing the gp practice weighted populations
     df = pd.read_csv(path)
-    df = df.rename(columns={"STP21_42": "ICS", "GP practice name": "practice_name"})
-    df["Practice"] = df["Practice"] + " " + ":" + " " + df["practice_name"]
+    df = df.rename(
+        columns={
+            "R21": "Region ODS code",
+            "Region21_7": "Region name",
+            "STP21": "ICS ODS code",
+            "STP21ons": "ICS ONS code",
+            "STP21_42": "ICS name",
+            "CCG": "CCG ODS code",
+            "CCG19": "CCG name",
+            "PCN": "PCN ODS code",
+            "PCN_name": "PCN code",
+            "Practice": "GP practice code",
+            "GP practice name": "GP practice name",
+            "GP_pop": "GP pop",
+            "WP_G&A": "Weighted G&A pop",
+            "WP_CS": "Weighted Community pop",
+            "WP_MH": "Weighted Mental Health pop",
+            "WP_Mat": "Weighted Maternity pop",
+            "WP_HCHS": "Weighted HCHS pop",
+            "WP_MFF": "Weighted Market Forces Factor pop",
+            "WP_EACA": "Weighted EACA pop",
+            "WP_Presc": "Weighted Prescribing pop",
+            "WP_AM": "Weighted AM pop",
+            "Target_exc_remote_£k": "Target exc remote (£k)",
+            "Target_inc_remote_£k": "Target inc remote (£k)",
+            "WP_Overall": "Overall Weighted pop",
+        }
+    )
+    df["practice_display"] = df["GP practice code"] + ": " + df["GP practice name"]
     return df
 
 
